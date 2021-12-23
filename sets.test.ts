f2169caa6ff824f9d43def47e4b7507446a1ca9b
Robert Philipp <rob.philipp@gmail.com>
2021-12-23 11:40:19 -0500

    ops and comparator

--- sets.test.ts
+++ sets.test.ts
@@ -1,33 +1,160 @@
-import {emptySet, withOps} from "./sets";
+import {emptySet, setFrom} from "./sets";
 
 test("should be able to create a set with operations", () => {
-    expect(withOps([1, 2, 3, 4]).size).toBe(4)
-    expect(withOps(new Set([1, 2, 3, 4, 5, 5, 6])).size).toBe(6)
+    expect(setFrom([1, 2, 3, 4]).size).toBe(4)
+    expect(setFrom(new Set([1, 2, 3, 4, 5, 5, 6])).size).toBe(6)
     expect(emptySet<number>().size).toBe(0)
 })
 
 test("should be able to calculate what's not in a set", () => {
-    const notIn = withOps([1, 2, 3, 4]).notIn(withOps([2, 3, 4]))
-    expect(notIn.toArray()).toEqual([1])
+    const A = setFrom([1, 2, 3, 4])
+    const B = setFrom([2, 3, 4])
+    const C = setFrom([2, 3, 4, 5])
+
+    // A\B (elements of A that are not in B)
+    expect(A.compliment(B).equals(setFrom([1]))).toBeTruthy()
+
+    // if B is a subset of A, then B\A (elements of B that are not in A) is an empty set
+    expect(B.isSubsetOf(A) && B.compliment(A).equals(emptySet())).toBeTruthy()
+
+    // when B is a proper subset of A, then B\A (elements of B that are not in A) is an empty set
+    expect(B.compliment(A).equals(emptySet())).toBeTruthy()
+
+    // when C is not a subset of A, then C\A (elements of C that are not in A) is not an empty set
+    expect(C.compliment(A).equals(setFrom([5]))).toBeTruthy()
+
+    expect(A.compliment(C).notEquals(C.compliment(A))).toBeTruthy()
+
+    // (empty set)\A = empty set
+    expect(emptySet().compliment(A).equals(emptySet())).toBeTruthy()
+
+    // A ∆ B = A\C union C\A = B ∆ A
+    expect(A.compliment(C).union(C.compliment(A)).equals(A.symmetricDifference(C))).toBeTruthy()
+    expect(A.compliment(C).union(C.compliment(A)).equals(C.symmetricDifference(A))).toBeTruthy()
 })
 
 test("should be able to map a set", () => {
-    const mapped = withOps([1, 2, 3, 4]).map(value => value * 2)
+    const mapped = setFrom([1, 2, 3, 4]).map(value => value * 2)
     expect(mapped.toArray()).toEqual([2, 4, 6, 8])
 })
 
-test("should be able to calculate the union and intersection of sets", () => {
-    const A = withOps([1, 2, 3, 4])
-    const B = withOps([3, 4, 5, 6, 7])
-    expect(A.union(B).toArray()).toEqual([1, 2, 3, 4, 5, 6, 7])
-    expect(A.intersection(B).toArray()).toEqual([3, 4])
+test("should be able to filter a set", () => {
+    const mapped = setFrom([1, 2, 3, 4]).filter(value => value % 2 === 0)
+    expect(mapped.toArray()).toEqual([2, 4])
+})
+
+test("should be able to calculate the union of sets", () => {
+    const A = setFrom([1, 2, 3, 4])
+    const B = setFrom([3, 4, 5, 6, 7])
+    const C = setFrom([1, 2, 3, 4, 5, 6, 7])
+    expect(A.union(B).equals(C)).toBeTruthy()
+
+    expect(A.union(B).equals(B.union(A))).toBeTruthy()
+    expect(A.isSubsetOf(A.union(B))).toBeTruthy()
+    expect(A.isProperSubsetOf(A.union(B))).toBeTruthy()
+    expect(A.union(A).equals(A)).toBeTruthy()
+    expect(A.union(emptySet()).equals(A)).toBeTruthy()
+
+    expect(A.isSubsetOf(B) && A.union(B).notEquals(B)).toBeFalsy()
+    expect(A.isSubsetOf(C) && A.union(C).equals(C)).toBeTruthy()
+})
+
+test("should be able to calculate the union of sets with comparator", () => {
+    const comparator = (a: [number, string], b: [number, string]) => a[0] === b[0] && a[1] === b[1]
+    const A = setFrom<[number, string]>([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd']], comparator)
+    const B = setFrom<[number, string]>([[3, 'c'], [4, 'd'], [5, 'e'], [6, 'f'], [7, 'g']], comparator)
+    const C = setFrom<[number, string]>([[1, 'a'], [2, 'b'], [3, 'c'], [4, 'd'], [5, 'e'], [6, 'f'], [7, 'g']], comparator)
+    expect(A.union(B).equals(C)).toBeTruthy()
+
+    expect(A.union(B).equals(B.union(A))).toBeTruthy()
+    expect(A.isSubsetOf(A.union(B))).toBeTruthy()
+    expect(A.isProperSubsetOf(A.union(B))).toBeTruthy()
+    expect(A.union(A).equals(A)).toBeTruthy()
+    expect(A.union(emptySet()).equals(A)).toBeTruthy()
+
+    expect(A.isSubsetOf(B) && A.union(B).notEquals(B)).toBeFalsy()
+    expect(A.isSubsetOf(C) && A.union(C).equals(C)).toBeTruthy()
+})
+
+test("should be able to calculate the intersection of sets", () => {
+    const A = setFrom([1, 2, 3, 4])
+    const B = setFrom([3, 4, 5, 6, 7])
+    const C = setFrom([1, 2, 3, 4, 5, 6, 7])
+
+    expect(A.intersection(B).equals(setFrom([3, 4]))).toBeTruthy()
+
+    // A intersection B must equal B intersection A
+    expect(A.intersection(B).equals(B.intersection(A))).toBeTruthy()
+
+    // A intersection B is a proper subset of A when A != B
+    expect(A.intersection(B).isProperSubsetOf(A)).toBeTruthy()
+    expect(A.intersection(A).isProperSubsetOf(A)).toBeFalsy()
+
+    // A intersection A must equal A
+    expect(A.intersection(A).equals(A)).toBeTruthy()
+
+    // A intersection with an empty set is an empty set
+    expect(A.intersection(emptySet()).equals(emptySet())).toBeTruthy()
+
+    // A is a subset of B iff A intersection B = A
+    expect(!A.isSubsetOf(B) && A.intersection(B).notEquals(A)).toBeTruthy()
+    expect(A.isSubsetOf(C) && A.intersection(C).equals(A)).toBeTruthy()
+
+})
+
+test("should be able to calculate the cartesian product of two sets", () => {
+    const comparator = (a: [number, string], b: [number, string]) => a[0] === b[0] && a[1] === b[1]
+    const A = setFrom([1, 2, 3, 4])
+    const B = setFrom(['one', 'two', 'three', 'four'])
+    expect(A.cartesianProduct(B, comparator).equals(setFrom([
+        [1, 'one'], [1, 'two'], [1, 'three'], [1, 'four'],
+        [2, 'one'], [2, 'two'], [2, 'three'], [2, 'four'],
+        [3, 'one'], [3, 'two'], [3, 'three'], [3, 'four'],
+        [4, 'one'], [4, 'two'], [4, 'three'], [4, 'four'],
+    ], comparator))).toBeTruthy()
 })
 
 test("should be able to add and remove elements from a set", () => {
     const set = emptySet<number>()
+    expect(set.isEmpty()).toBeTruthy()
+    expect(set.nonEmpty()).toBeFalsy()
     expect(set.add(3).toArray()).toEqual([3])
     expect(set.add(1).toArray()).toEqual([3, 1])
     expect(set.add(5).toArray()).toEqual([3, 1, 5])
     expect(set.delete(5)).toBeTruthy()
     expect(set.add(4).add(1).add(5).add(9).toArray()).toEqual([3, 1, 4, 5, 9])
+    expect(set.isEmpty()).toBeFalsy()
+    expect(set.nonEmpty()).toBeTruthy()
+})
+
+test("should be able to test subset and equality", () => {
+    const setA = setFrom([1,2,3,4,5,6])
+    const setB = setFrom([3,4,6,7,8,10])
+    const setC = setA.intersection(setB)
+
+    // a set should equal itself
+    expect(setA.equals(setA)).toBeTruthy()
+    expect(setA.notEquals(setA)).toBeFalsy()
+
+    // two unequal sets should not be equal
+    expect(setA.equals(setB)).toBeFalsy()
+    expect(setA.notEquals(setB)).toBeTruthy()
+
+    // the intersection of to unequal sets, A and B, is a proper subset of both sets A and B
+    expect(setC.isSubsetOf(setA)).toBeTruthy()
+    expect(setC.isSubsetOf(setB)).toBeTruthy()
+    expect(setC.isProperSubsetOf(setA)).toBeTruthy()
+    expect(setC.isProperSubsetOf(setB)).toBeTruthy()
+
+    // as set is a subset of itself, but not a proper subset of itself
+    expect(setA.isSubsetOf(setA)).toBeTruthy()
+    expect(setA.isProperSubsetOf(setA)).toBeFalsy()
+
+    // an empty set is a subset of every set
+    expect(emptySet<number>().isProperSubsetOf(setA)).toBeTruthy()
+    expect(emptySet<number>().isProperSubsetOf(setB)).toBeTruthy()
+
+    // an empty set is a subset of an empty set, but not a proper subset of an empty set
+    expect(emptySet().isProperSubsetOf(emptySet())).toBeFalsy()
+    expect(emptySet().isSubsetOf(emptySet())).toBeTruthy()
 })
--- sets.ts
+++ sets.ts
@@ -3,39 +3,148 @@ export interface WithOps<T> extends Set<T> {
     toArray: () => Array<T>
 
     map: (callback: (value: T, index: number, s: SetLike<T>) => T) => WithOps<T>,
+    filter: (callback: (value: T, index: number, s: SetLike<T>) => boolean) => WithOps<T>,
 
-    notIn: (b: SetLike<T>) => WithOps<T>
+    /**
+     * Calculates the elements of this set that are not in the set `b`. For example, if
+     * this set is denoted as `A` and the other set is denoted as `B`, then `A` compliment `B`,
+     * or `A\B`, are the elements of `A` that are **not** in `B`.
+     * @param b The other set
+     * @return The elements of this set that are not in `b`
+     */
+    compliment: (b: SetLike<T>) => WithOps<T>
+    /**
+     * Calculates the symmetric difference between this set and the other set. For example, if
+     * this set is denoted as `A` and the other set is denoted as `B`, then `A` symmetric difference `B`,
+     * or `A ∆ B`, is the union of the set making up the elements of `A` that are **not** in `B` **and**
+     * the elements of `B` that are **not** in `A`. Or `A ∆ B = (A\B) ∪ (B\A) = B ∆ A`.
+     * @param b The other set
+     * @return The union of the elements of this set that are not in `b` and the elements of `b`
+     * that are not in this set.
+     */
+    symmetricDifference: (b: SetLike<T>) => WithOps<T>
+    /**
+     * Calculates the elements of this set that are also in the other set, `b`. For example, if this
+     * set is denoted as `A` and the other set as `B`, then `A` intersection `B`, or `A ∩ B`, are the
+     * set of elements that are in both sets.
+     * @param b The other set
+     * @return the elements of this set that are also in the set `b`
+     */
     intersection: (b: SetLike<T>) => WithOps<T>
+    /**
+     * Calculates a new set consisting of the elements of this set and all the elements in the other set.
+     * For example, if this set is denoted as `A` and the other set as `B`, then `A` union `B`, or
+     * `A ∪ B`, is the set of all elements in `A` and all the elements in `B`.
+     * @param b The other set
+     * @return a new set consisting of the elements of this set and all the elements in the other set
+     */
     union: (b: SetLike<T>) => WithOps<T>
+    /**
+     * Calculates the cartesion product of this set with the other set. For example, if this set is denoted
+     * by `A` and the other set by `B`, then `A` cartesian product `B`, or `A × B`, is the set of all the
+     * possible element pairs
+     * @param b
+     */
+    cartesianProduct: <B>(b: SetLike<B>, comparator: (a: [T, B], b: [T, B]) => boolean) => WithOps<[T, B]>
+    /**
+     * Calculates if the other set has all the elements found in this set. For example, if this set is
+     * denoted as `A` and the other set is denoted as `B`, then `A` is subset of `B`, or `A ⊆ B`, evaluates
+     * to `true` if `B` contains, at least, all the elements of `A`.
+     * @param b The other set
+     * @return `true` if the other set has (at least) all the elements found in this set; `false` otherwise
+     */
+    isSubsetOf: (b: SetLike<T>) => boolean
+    /**
+     * Calculates if the other set has all the elements found in this set, and the other set does not equal
+     * this set. For example, if this set is denoted as `A` and the other set is denoted as `B`, then
+     * `A` is a proper subset of `B`, or `A ⊂ B`, evaluates to `true` if `B` contains all the elements of `A`,
+     * **and** `A ≠ B`.
+     * @param b The other set
+     * @return `true` if the other set has all the elements found in this set, and the other set does not
+     * equal this set; `false` otherwise
+     */
+    isProperSubsetOf: (b: SetLike<T>) => boolean
+
+    /**
+     * This set equals the other set if and only if the two sets have exactly the same elements, no more,
+     * and no less. For example, if this set is denoted as `A` and the other set is `B`, then the set are
+     * equal `iff` `A ∪ B = A ⊂ B`.
+     * @param b The other set
+     * @return `true` if and only if this set and the other set share exactly the same elements; `false`
+     * otherwise.
+     * @see notEquals
+     */
+    equals: (b: SetLike<T>) => boolean
+    /**
+     * This set does not equal the other set if the two sets do not have exactly the same elements. For
+     * example, if this set is denoted as `A` and the other set is `B`, then the set are not equal if
+     * `A ∪ B ≠ A ⊂ B`.
+     * @param b The other set
+     * @return `true` if this set and the other set do not share exactly the same elements; `false`
+     * otherwise.
+     * @see equals
+     */
+    notEquals: (b: SetLike<T>) => boolean
+
+    isEmpty: () => boolean
+    nonEmpty: () => boolean
+
+    cardinality: number
 }
 
 type SetLike<T> = Set<T> | WithOps<T>
 type Collection<T> = SetLike<T> | ArrayLike<T>
 
 export function emptySet<T>(): WithOps<T> {
-    return withOps<T>([])
+    return setFrom<T>([])
 }
 
-export function withOps<T>(collection: Collection<T>): WithOps<T> {
+export function setFrom<T>(collection: Collection<T>, comparator?: (a: T, b: T) => boolean): WithOps<T> {
     const set = convertToSet(collection)
 
     function map(callback: (value: T, index: number, s: SetLike<T>) => T): WithOps<T> {
         const fn = (value: T, index: number, array: Array<T>) => callback(value, index, convertToSet(array))
-        return withOps(convertToArray(set).map(fn))
+        return setFrom(convertToArray(set).map(fn), comparator)
     }
 
-    function notIn(b: SetLike<T>): WithOps<T> {
-        return withOps(convertToArray(set).filter(elemA => !b.has(elemA)))
+    function filter(callback: (value: T, index: number, s: SetLike<T>) => boolean): WithOps<T> {
+        const fn = (value: T, index: number, array: Array<T>) => callback(value, index, convertToSet(array))
+        return setFrom(convertToArray(set).filter(fn), comparator)
+    }
+
+    function compliment(b: SetLike<T>): WithOps<T> {
+        return setFrom(calculateNotIn(set, b, comparator), comparator)
+    }
+
+    function symmetricDifference(b: SetLike<T>): WithOps<T> {
+        return setFrom(calculateUnion(calculateNotIn(set, b, comparator), calculateNotIn(b, set, comparator)), comparator)
     }
 
     function intersection(b: SetLike<T>): WithOps<T> {
-        return withOps(convertToArray(set).filter(elemA => b.has(elemA)))
+        return setFrom(calculateIntersection(set, b, comparator), comparator)
     }
 
     function union(b: SetLike<T>): WithOps<T> {
-        const newSet = new Set(set)
-        b.forEach(elemB => newSet.add(elemB))
-        return withOps(newSet)
+        return setFrom(calculateUnion(set, b, comparator), comparator)
+    }
+
+    function cartesianProduct<B>(b: SetLike<B>, comparator: (a: [T, B], b: [T, B]) => boolean): WithOps<[T, B]> {
+        return setFrom<[T, B]>(calculateCartesianProduct<T, B>(set, b), comparator)
+    }
+
+    function isSubsetOf(b: SetLike<T>): boolean {
+        const inter = calculateIntersection(set, b, comparator)
+        return inter.size === set.size
+    }
+
+    function isProperSubsetOf(b: SetLike<T>): boolean {
+        const inter = calculateIntersection(set, b, comparator)
+        return inter.size === set.size && inter.size < b.size
+    }
+
+    function equals(b: SetLike<T>): boolean {
+        const inter = calculateIntersection(set, b, comparator)
+        return inter.size === set.size && inter.size === b.size
     }
 
     return {
@@ -43,14 +152,27 @@ export function withOps<T>(collection: Collection<T>): WithOps<T> {
         toArray: () => convertToArray(set),
 
         map,
+        filter,
 
-        notIn,
+        isEmpty: () => set.size === 0,
+        nonEmpty: () => set.size > 0,
+
+        compliment,
+        symmetricDifference,
         intersection,
         union,
+        cartesianProduct,
+        isSubsetOf,
+        isProperSubsetOf,
+
+        equals,
+        notEquals: b => !equals(b),
+
+        cardinality: set.size,
 
         [Symbol.iterator]: () => set.values(),
         [Symbol.toStringTag]: "set",
-        add: (value: T) => withOps(set.add(value)),
+        add: (value: T) => setFrom(set.add(value)),
         delete: (value: T) => set.delete(value),
         has: (value: T) => set.has(value),
         size: set.size,
@@ -77,4 +199,76 @@ function convertToArray<T>(collection: Collection<T>): Array<T> {
         return [...collection]
     }
     return [...convertToSet(collection)]
+}
+
+function calculateIntersection<T>(setA: Set<T>, setB: SetLike<T>, comparator?: (a: T, b: T) => boolean): Set<T> {
+    const intersection = new Set<T>()
+    if (comparator !== undefined) {
+        for (let elemA of setA) {
+            for (let elemB of setB) {
+                if (comparator(elemA, elemB)) {
+                    intersection.add(elemA)
+                    break
+                }
+            }
+        }
+        return intersection
+    }
+    for (let elemA of setA) {
+        if (setB.has(elemA)) {
+            intersection.add(elemA)
+        }
+    }
+    return intersection
+}
+
+function calculateUnion<T>(setA: Set<T>, setB: SetLike<T>, comparator?: (a: T, b: T) => boolean): Set<T> {
+    const union = new Set<T>(setA)
+    if (comparator !== undefined) {
+        for (let elemB of setB) {
+            let found = false
+            for (let elemA of setA) {
+                if (comparator(elemA, elemB)) {
+                    found= true
+                    break
+                }
+            }
+            if (!found) {
+                union.add(elemB)
+            }
+        }
+        return union
+    }
+    setB.forEach(elem => union.add(elem))
+    return union
+}
+
+function calculateNotIn<T>(setA: Set<T>, setB: SetLike<T>, comparator?: (a: T, b: T) => boolean): Set<T> {
+    const notIn = new Set<T>()
+    if (comparator !== undefined) {
+        for (let elemA of setA) {
+            for (let elemB of setB) {
+                if (!comparator(elemA, elemB)) {
+                    notIn.add(elemA)
+                }
+            }
+        }
+        return notIn
+    }
+    for (let elemA of setA) {
+        if (!setB.has(elemA)) {
+            notIn.add(elemA)
+        }
+    }
+    return notIn
+}
+
+function calculateCartesianProduct<T, B>(setA: Set<T>, setB: SetLike<B>): Set<[T, B]> {
+    const product = new Set<[T, B]>()
+    for (let elemA of setA) {
+        for (let elemB of setB) {
+            product.add([elemA, elemB] as [T, B])
+        }
+    }
+    return product
 }
